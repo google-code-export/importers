50f1f6d20a719b5a6ead35fd381ae42190f60f8b
Brett Cannon <brett@python.org>
2009-11-30 21:19:01 -0800

    Start hook tests. Only vreifying the simplest case to start.

---
+++ importers/test/test_sqlite3.py
@@ -0,0 +1,51 @@
+from .. import sqlite3 as importer
+import contextlib
+import os
+import shutil
+import sqlite3
+import tempfile
+import unittest
+
+
+@contextlib.contextmanager
+def TestDB():
+    directory = tempfile.mkdtemp()
+    try:
+        path = os.path.join(directory, 'importers_test.db')
+        cxn = sqlite3.connect(path)
+        with cxn:
+            # XXX Should it be TEXT or something else?
+            cxn.execute('''CREATE TABLE PythonCode
+                           (path PRIMARY KEY, py, pyc, pyo)''')
+        cxn.close()
+        yield path
+    finally:
+        shutil.rmtree(directory)
+
+
+class HookTest(unittest.TestCase):
+
+    def test_direct_path(self):
+        # A direct path to a sqlite3 DB w/ the proper format should return a
+        # finder.
+        with TestDB() as path:
+            hook = importer.Hook()
+            finder = hook(path)
+
+    def _test_indirect_path(self):
+        # A path containing a DB plus a package directory should retrn a
+        # finder.
+        raise NotImplementedError
+
+    def _test_bad_path(self):
+        # A path not containing a DB should raise ImportError.
+        raise NotImplementedError
+
+
+def main():
+    from test.support import run_unittest
+    run_unittest(HookTest)
+
+
+if __name__ == '__main__':
+    main()
