2135ecfc955f40617eb1fbbf7a58317e87eac780
Brett Cannon <brett@python.org>
2009-12-06 14:29:42 -0800

    Add a comment about the realization that trying to be clever w/ the DB is not working. Thanks Doug Hellman for helping me reach this realization.

--- importers/sqlite3.py
+++ importers/sqlite3.py
@@ -1,3 +1,4 @@
+# XXX Scrap and redo; basically need to make it a generic FS impl.
 sql_creation = [
     '''CREATE TABLE PythonCode
          (path TEXT PRIMARY KEY, py BLOB, pyc BLOB, pyo BLOB);''',
@@ -24,6 +25,7 @@ been passed to the interpreter. Both columns are stored as bytes.
 
 """.format(sql='  \n'.join(sql_creation))
 
+import collections
 import importlib.abc
 from importlib._bootstrap import _check_name  # XXX VERY VERY NAUGHTY!
 import os
@@ -145,33 +147,90 @@ class Loader(importlib.abc.PyPycLoader):
         self._name = name
         self._path = path
 
+    def fetch_code(self):
+        """Return the source and bytecode (if any) for the path the loader was
+        created for."""
+        sql_select = ('SELECT py, py{} FROM PythonCode '
+                      'WHERE path=?'.format('c' if __debug__ else 'o'))
+
+        with self._cxn:
+            cursor = self._cxn.execute(sql_select, [self._path])
+            return tuple(cursor)
 
     @_check_name
     def source_path(self, fullname):
-        # XXX Raise ImportError if source not in the db
-        raise NotImplementedError
+        """Return the source 'path' to the module if the source is available,
+        return None if bytecode exists, else raise ImportError.
+
+        A two-item tuple is returned for source path where the second item is
+        the string 'py'.
+
+        """
+        source, bytecode = self.fetch_code()
+        if source:
+            return (self._path, 'py')
+        elif bytecode:
+            return None
+        else:
+            raise ImportError('{} cannot be handled by this '
+                                'loader'.format(fullname))
 
     def get_data(self, path):
-        # XXX return the source or bytecode (do based on file extension)
-        raise NotImplementedError
+        """Return data for the path.
+
+        Source and bytecode paths are represented by a two-item tuple. The
+        first item is the path to the module being loaded. The second item
+        specifies whether the path represents source or bytecode.
+
+        """
+        if (isinstance(path, collections.Sequence) and len(path) == 2 and
+                path[0] == self.__path and path[1] in {'py', 'pyc', 'pyo'}):
+            source, bytecode = self.fetch_code()
+            if path[1] == 'py':
+                return source
+            if path[1] == 'pyc' and __debug__:
+                return bytecode
+            elif path[1] == 'pyo' and not __debug__:
+                return bytecode
+        # Fall-through failure case.
+        raise IOError("the path {!r} does not exist".format(path))
 
     @_check_name
     def is_package(self, fullname):
-        # XXX Base it on the path and if it ends in __init__
-        raise NotImplementedError
+        """Return if the module is a package based on whether the path ends in
+        '/__init__'."""
+        return self._path.endswith('/__init__')
 
     @_check_name
     def source_mtime(self, fullname):
-        # XXX checks
+        """Return 1 for the source mtime.
+
+        Database trigger should guarantee that if the 'py' column is changed
+        for a row that the 'pyc' and 'pyo' columns are set to NULL.
+
+        """
         return 1
 
     @_check_name
     def bytecode_path(self, fullname):
-        # XXX
-        raise NotImplementedError
+        """Return the path to the bytecode of the module (if present), None if
+        source is present, or raise ImportError.
+
+        A two-item tuple is used to represent the path. The first item is the
+        path itself with the second item being either 'pyc' or 'pyo' depending
+        on whether __debug__ is true.
+
+        """
+        source, bytecode = self.fetch_code()
+        if bytecode:
+            return (self._path, 'py{}'.format('c' if __debug__ else 'o'))
+        elif source:
+            return None
+        else:
+            raise ImportError('{} is not handled by this '
+                                'loader'.format(fullname))
 
     @_check_name
     def write_bytecode(self, fullname, bytecode):
-        # XXX
-        raise NotImplementedError
-
+        """Write the bytecode into the database."""
+        bc_column = 'py{}'.format('c' if __debug__ else 'o')
