3524b5a038f2869661ca3fda4bcf67c6476ce5be
Brett Cannon <brett@python.org>
2009-12-16 23:09:50 -0800

    Move util testing code to own module.

--- importers/test/test_sqlite3.py
+++ importers/test/test_sqlite3.py
@@ -1,4 +1,5 @@
 from .. import sqlite3 as importer
+from . import util
 import contextlib
 import os
 import shutil
@@ -65,78 +66,7 @@ class Sqlite3HookTest(unittest.TestCase):
             self.assertTrue(isinstance(finder, importer.Importer))
 
 
-class PyFileLoaderTest(unittest.TestCase):
-
-    """Superclass for testing py file loaders.
-
-    Subclasses must provide the following attributes:
-
-        * base_path
-            Base path where created files are kept (e.g a directory or archive
-            file).
-        * importer
-            The loader. It must contain a file relative to
-            self.relative_file_path with the value of self.data. The location
-            of the loader should be self.location.
-
-    """
-
-    location = 'pkg'
-    relative_file_path = os.path.join('pkg', 'module.py')
-    data = b'fake = True'
-
-    def test_file_exists(self):
-        # Test that file_exists returns true for existing paths and false
-        # otherwise.
-        path = os.path.join(self.base_path, self.relative_file_path)
-        self.assertTrue(self.importer.file_exists(path))
-        self.assertFalse(self.importer.file_exists('nothing'))
-
-    def test_get_data(self):
-        # Should return the data that is stored.
-        path = os.path.join(self.base_path, self.relative_file_path)
-        self.assertEqual(self.importer.get_data(path), self.data)
-
-    def test_load_module(self):
-        # Integration test for load_module().
-        module = self.importer.load_module('pkg.module')
-        try:
-            self.assertTrue(hasattr(module, 'fake'))
-            self.assertEqual(module.fake, True)
-        finally:
-            del sys.modules['pkg.module']
-
-
-class PyPycFileLoaderTest(PyFileLoaderTest):
-
-    """Superclass for testing py/pyc file loaders.
-
-    Subclasses must provide the following attributes in addition to the one's
-    required by the subclass:
-
-        * mtime
-            The modification time for the file.
-        * mutable
-            True/false value indicating if the loader can write data.
-
-    """
-
-    def test_path_mtime(self):
-        # Should return the proper mtime.
-        path = os.path.join(self.base_path, self.relative_file_path)
-        self.assertEqual(self.importer.path_mtime(path), self.mtime)
-
-    def test_write_data(self):
-        # Should write the data to the DB.
-        if not self.mutable:
-            self.skip("loader must support file mutation")
-        path = os.path.join(self.base_path, self.relative_file_path)
-        new_data = b'fake = False'
-        self.assertTrue(self.importer.write_data(path, new_data))
-        self.assertEqual(self.importer.get_data(path), new_data)
-
-
-class Sqlite3ImporterTest(PyPycFileLoaderTest):
+class Sqlite3ImporterTest(util.PyFileFinderTest, util.PyPycFileLoaderTest):
 
     mutable = True
 
@@ -160,11 +90,6 @@ class Sqlite3ImporterTest(PyPycFileLoaderTest):
     def test_loader(self):
         # Returns self.
         self.assertIs(self.importer, self.importer.loader())
-
-    def test_find_module(self):
-        # Integration test for find_module.
-        loader = self.importer.find_module('pkg.module')
-        self.assertIsNotNone(loader)
 
 
 def main():
---
+++ importers/test/util.py
@@ -0,0 +1,99 @@
+import os
+import sys
+import unittest
+
+
+class PyFileFinderTest(unittest.TestCase):
+
+    """Superclass for testing py file finders.
+
+    Do not that no tests are performed for the loader() method on finders as
+    they are very finder-specific.
+
+    Subclasses must provide the following attributes:
+
+        * base_path
+            Base path where created files are kept (e.g a directory or archive
+            file).
+        * importer
+            The finder. It must contain a file relative to
+            self.relative_file_path with the value of self.data. The location
+            of the loader should be self.location.
+
+    """
+
+    def test_file_exists_for_finder(self):
+        # Test that file_exists returns true for existing paths and false
+        # otherwise.
+        path = os.path.join(self.base_path, self.relative_file_path)
+        self.assertTrue(self.importer.file_exists(path))
+        self.assertFalse(self.importer.file_exists('nothing'))
+
+    def test_find_module(self):
+        # Integration test for find_module.
+        loader = self.importer.find_module('pkg.module')
+        self.assertIsNotNone(loader)
+
+
+class PyFileLoaderTest(unittest.TestCase):
+
+    """Superclass for testing py file loaders.
+
+    Subclasses must provide the same attributes as PyFileFinderTest requires
+    except self.importer is set to the loader.
+
+    """
+
+    location = 'pkg'
+    relative_file_path = os.path.join('pkg', 'module.py')
+    data = b'fake = True'
+
+    # Re-use the finder file_exists() tests.
+    test_file_exists_for_loader = PyFileFinderTest.test_file_exists_for_finder
+
+    def test_get_data(self):
+        # Should return the data that is stored.
+        path = os.path.join(self.base_path, self.relative_file_path)
+        self.assertEqual(self.importer.get_data(path), self.data)
+
+    def test_load_module(self):
+        # Integration test for load_module().
+        module = self.importer.load_module('pkg.module')
+        try:
+            self.assertTrue(hasattr(module, 'fake'))
+            self.assertEqual(module.fake, True)
+        finally:
+            del sys.modules['pkg.module']
+
+class PyPycFileLoaderTest(PyFileLoaderTest):
+
+    """Superclass for testing py/pyc file loaders.
+
+    Subclasses must provide the following attributes in addition to the one's
+    required by the subclass:
+
+        * mtime
+            The modification time for the file.
+        * mutable
+            True/false value indicating if the loader can write data.
+
+    """
+
+    def test_path_mtime(self):
+        # Should return the proper mtime.
+        path = os.path.join(self.base_path, self.relative_file_path)
+        self.assertEqual(self.importer.path_mtime(path), self.mtime)
+
+    def test_write_data(self):
+        # Should write the data to the DB.
+        if not self.mutable:
+            self.skip("loader must support file mutation")
+        path = os.path.join(self.base_path, self.relative_file_path)
+        new_data = b'fake = False'
+        self.assertTrue(self.importer.write_data(path, new_data))
+        self.assertEqual(self.importer.get_data(path), new_data)
+
+    # XXX protect w/ writes_bytecode_file decorator
+    def _test_load_module_w_bytecode(self):
+        # XXX
+        self.fail()
