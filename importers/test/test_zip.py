ec7cb34705abff71a60749df6f7f6dfd89890925
Brett Cannon <brett@python.org>
2009-12-18 17:14:24 -0800

    Fill in tests for the zip hook.

--- importers/test/test_zip.py
+++ importers/test/test_zip.py
@@ -6,11 +6,44 @@ import tempfile
 import unittest
 import zipfile
 
+
+def create_zip(file_path, data):
+    """Create a zip file containing a file."""
+    directory = tempfile.mkdtemp()
+    base_path = os.path.join(directory, 'archive.zip')
+    zip_ = zipfile.ZipFile(base_path, 'w')
+    zip_.writestr(file_path, data)
+    zip_.close()
+    return base_path
+
+
 class ZipHookTest(unittest.TestCase):
 
     """Test importers.zip.Hook."""
 
-    pass
+    def setUp(self):
+        self.path = create_zip('module.py', b'fake = True')
+        self.hook = importer.Hook()
+
+    def tearDown(self):
+        shutil.rmtree(os.path.dirname(self.path))
+
+    def test_zip(self):
+        # Finding a zip file directly specified.
+        found = self.hook.open(self.path)
+        self.assertTrue(isinstance(found, zipfile.ZipFile))
+
+    def test_non_zip(self):
+        # Finding a file that is not a zipfile should fail.
+        with open(self.path) as file:
+            file.write('abcd')
+        with self.assertRaises(ValueError):
+            self.hook.open(self.path)
+
+    def test_finder(self):
+        zip_ = self.hook.open(self.path)
+        finder = self.hook.finder(zip_, self.path, '')
+        self.assertTrue(isinstance(finder, importer.Importer))
 
 
 class ZipImporterTest(util.PyFileFinderTest, util.PyFileLoaderTest):
@@ -18,18 +51,12 @@ class ZipImporterTest(util.PyFileFinderTest, util.PyFileLoaderTest):
     """Test importers.zip.Importer."""
 
     def setUp(self):
-        # XXX self.base_path
-        # XXX self.importer
-        self._directory = tempfile.mkdtemp()
-        self.base_path = os.path.join(self._directory, 'archive.zip')
-        zip_ = zipfile.ZipFile(self.base_path, 'w')
-        zip_.writestr(self.relative_file_path, self.data)
-        zip_.close()
+        self.base_path = create_zip(self.relative_file_path, self.data)
         zip_ = zipfile.ZipFile(self.base_path)
         self.importer = importer.Importer(zip_, self.base_path, self.location)
 
     def tearDown(self):
-        shutil.rmtree(self._directory)
+        shutil.rmtree(os.path.dirname(self.base_path))
 
     def test_loader(self):
         # Should return self.
