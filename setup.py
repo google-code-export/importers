87ef71f54cb6f386898a3905769f7bc8398cee00
Brett Cannon <brett@python.org>
2010-01-16 18:57:09 -0800

    Add setup.py file.

---
+++ setup.py
@@ -0,0 +1,16 @@
+#from distutils.core import setup
+from setuptools import setup
+
+setup(name='importers',
+      version='0.1',
+      description='Various importers and utility code for writing importers',
+      author='Brett Cannon',
+      author_email='brett@python.org',
+      url='http://importers.googlecode.com',
+      packages=['importers'],
+      classifiers=['Development Status :: 4 - Beta',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.1',
+          ],
+     )
