5e3613778611f60440d9cf236e548b73578e78ed
Brett Cannon <brett@python.org>
2011-06-08 20:35:56 -0700

    Flesh out tests that should _eventually_ pass.

--- importers/test/test_lazy.py
+++ importers/test/test_lazy.py
@@ -18,17 +18,17 @@ class MockLoader(importlib.abc.Loader):
         self.loaded = False
 
     def load_module(self, fullname):
+        """As the lazy loader will have already put the module in sys.modules,
+        simply return that and modify as necessary as it will be loaded at this
+        point."""
         self.loaded = True
-        try:
-            return sys.modules[fullname]
-        except KeyError:
-            module = imp.new_module(fullname)
-            sys.modules[fullname] = module
-            return module
+        module = sys.modules[fullname]
+        module.attr = None
+        return module
 
 
 class MockLazyLoader(lazy.Mixin, MockLoader):
-
+    """Class mixing the mock loader with the lazy mixin."""
     pass
 
 
@@ -37,8 +37,9 @@ class LazyMixinTest(unittest.TestCase):
     """Test importer.lazy.Mixin."""
 
     def setUp(self):
+        self.name = '_lazy_test_mdoule'
         self.loader = MockLazyLoader()
-        self.name = 'module'
+        self.tearDown()
 
     def tearDown(self):
         try:
@@ -50,24 +51,50 @@ class LazyMixinTest(unittest.TestCase):
         # Accessing an attribute should trigger a load.
         module = self.loader.load_module(self.name)
         self.assertFalse(self.loader.loaded)
-        module.__name__
+        self.assertFalse(module.attr)  # Triggers load
         self.assertTrue(self.loader.loaded)
         self.assertFalse(isinstance(module.__loader__, MockLazyLoader))
+        # Since __loader__ is now an instance of super(), indirect ways must be
+        # used to verify the right thing occurred.
+        assert MockLazyLoader.__doc__ != MockLoader.__doc__
+        self.assertEqual(module.__loader__.__doc__, MockLoader.__doc__)
         self.assertEqual(module.__class__.__getattribute__,
                             types.ModuleType.__getattribute__)
 
-    def test__getattribute__access(self):
-        # Accessing __getattribute__ itself should trigger the load.
+    def test__getattribute__read(self):
+        # Accessing __getattribute__ itself (or any attribute) should trigger
+        # the load.
         module = self.loader.load_module(self.name)
         self.assertFalse(self.loader.loaded)
         module.__getattribute__
         self.assertTrue(self.loader.loaded)
 
-    def test_reload(self):
-        # A reload should trigger an immediate load.
-        sys.modules[self.name] = imp.new_module(self.name)
+    @unittest.expectedFailure
+    def test_attr_write(self):
+        # Setting an attribute should trigger the load **before** the attribute
+        # is actually mutated.
         module = self.loader.load_module(self.name)
+        self.assertFalse(self.loader.loaded)
+        module.attr = True
+        self.assertTrue(self.loader.loaded,
+                        'module not loaded after an attribute assignment')
+        self.assertTrue(module.attr)
+
+    @unittest.expectedFailure
+    def test_imp_reload(self):
+        # A reload should trigger an immediate load.
+        module = self.loader.load_module(self.name)
+        self.assertFalse(self.loader.loaded)
+        # XXX failing because there is no finder to return the lazy loader
+        imp.reload(module)
         self.assertTrue(self.loader.loaded)
+
+    def test_manual_reload(self):
+        # Calling __loader__.load_module() should work w/o issue.
+        module = self.loader.load_module(self.name)
+        module.attr = True
+        module.__loader__.load_module(self.name)  # Reset 'attr' to None
+        self.assertFalse(module.attr)
 
 
 def main():
